7b2e61b343b05e39c3c8e751208f8b10de20622c
Rohan <yadav.gunwal.rohan@gmail.com>
2022-11-23 21:09:14 +0530

    some changes

---
+++ server/routes/orderRoutes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+const orderController = require("../controllers/orderController");
+
+router.get("/", orderController.getAllOrders);
+router.post("/", orderController.createOrder);
+router.delete("/:id", orderController.deleteOrder);
+
+module.exports = router;
